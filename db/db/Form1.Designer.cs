811f27da6aa4f5a5fde0301b991c107af5f12551
Svalka403 <mojorik@inbox.ru>
2016-10-29 10:48:18 +0300

    Add files via upload

---
+++ db/db/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace db
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(713, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 404);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "select * from groups";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(375, 404);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(881, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(393, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 394);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 462);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ db/db/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+using System.Data.Common;
+
+namespace db
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string a; string b;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            String CS = "Server=spsu.ru;Port=5433;User=shtyrba;Password=gBTnz9;Database=goshchina;";
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(CS);
+            npgSqlConnection.Open();
+            a = textBox1.Text;
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(a, npgSqlConnection);
+            NpgsqlCommandBuilder cd = new NpgsqlCommandBuilder(da);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "grousdssds");
+
+
+            dataGridView1.DataSource = ds.Tables[0];
+            npgSqlConnection.Close();
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            string asd = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+            string str = dataGridView1.Columns[e.ColumnIndex].Name.ToString();
+            if (str=="group_name"||str=="abbr_name")
+            {
+                asd="\'"+asd+"\'";
+            }
+            String connectionString = "Server=spsu.ru;Port=5433;User=shtyrba;Password=gBTnz9;Database=goshchina;";
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+            b = String.Format ("select students.id,students.students_name,period,group_id,abbr_name from students,studentsgroup,groups where students.id=studentsgroup.students_id and groups.id=studentsgroup.group_id and groups.{1} ={0}",asd,str);
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(b, npgSqlConnection);
+            NpgsqlCommandBuilder cd = new NpgsqlCommandBuilder(da);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+           // textBox1.Text = b;
+            listBox1.DataSource = ds.Tables[0].Columns[0];
+            npgSqlConnection.Close();
+        }
+
+       
+
+    }
+}
